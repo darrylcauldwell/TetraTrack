64b81699a7ff39599a5e3c87384d37bdd1bdeb05
Darryl Cauldwell <darryl_cauldwell@hotmail.com>
2026-03-19 23:25:29 +0000

    fix: update TetraTrackShared platform minimums to iOS 26 / watchOS 26 #261

--- TetraTrackShared/Package.swift
+++ TetraTrackShared/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "TetraTrackShared",
     platforms: [
-        .iOS(.v18),
-        .watchOS(.v11)
+        .iOS(.v26),
+        .watchOS(.v26)
     ],
     products: [
         .library(
